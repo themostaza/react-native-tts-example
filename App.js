2a47627fc8d1eeadcdc3037abd4a991d3e0a70fd
Andrea Tosatto <andrea@themostaza.com>
2018-06-12 14:32:36 +0200

    Tested on Android device

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  * @flow
  */
 
@@ -12,7 +10,9 @@ import {
   View,
   Button,
   FlatList,
-  Slider
+  Slider,
+  TextInput,
+  Keyboard
 } from "react-native";
 import Tts from "react-native-tts";
 
@@ -20,7 +20,7 @@ type Props = {};
 export default class App extends Component<Props> {
   state = {
     voices: [],
-    ttsStatus: null,
+    ttsStatus: "initiliazing",
     selectedVoice: null,
     speechRate: 0.5,
     speechPitch: 1,
@@ -38,37 +38,60 @@ export default class App extends Component<Props> {
     Tts.addEventListener("tts-cancel", event =>
       this.setState({ ttsStatus: "cancelled" })
     );
-    Tts.voices().then(voices => {
-      const availableVoices = voices
-        .filter(v => !v.networkConnectionRequired && !v.notInstalled)
-        .map(v => {
-          return { id: v.id, name: v.name, language: v.language };
-        });
-      this.setState({ voices: availableVoices });
-    });
+    Tts.setDefaultRate(this.state.speechRate);
+    Tts.setDefaultPitch(this.state.speechPitch);
+    Tts.getInitStatus().then(this.initTts);
   }
 
-  readText = async () => {
-    if (!this.state.ttsStatus) {
-      await Tts.getInitStatus();
+  initTts = async () => {
+    const voices = await Tts.voices();
+    const availableVoices = voices
+      .filter(v => !v.networkConnectionRequired && !v.notInstalled)
+      .map(v => {
+        return { id: v.id, name: v.name, language: v.language };
+      });
+    let selectedVoice = null;
+    if (voices && voices.length > 0) {
+      selectedVoice = voices[0].id;
+      try {
+        await Tts.setDefaultLanguage(voices[0].language);
+      } catch (err) {
+        // My Samsung S9 has always this error: "Language is not supported"
+        console.log(`setDefaultLanguage error `, err);
+      }
+      await Tts.setDefaultVoice(voices[0].id);
+      this.setState({
+        voices: availableVoices,
+        selectedVoice,
+        ttsStatus: "initialized"
+      });
+    } else {
       this.setState({ ttsStatus: "initialized" });
     }
-    await Tts.stop();
-    await Tts.speak(this.state.text);
+  };
+
+  readText = async () => {
+    Tts.stop();
+    Tts.speak(this.state.text);
   };
 
   setSpeechRate = async rate => {
-    this.setState({ speechRate: rate });
     await Tts.setDefaultRate(rate);
+    this.setState({ speechRate: rate });
   };
 
   setSpeechPitch = async rate => {
-    this.setState({ speechPitch: rate });
     await Tts.setDefaultPitch(rate);
+    this.setState({ speechPitch: rate });
   };
 
   onVoicePress = async voice => {
-    await Tts.setDefaultLanguage(voice.language);
+    try {
+      await Tts.setDefaultLanguage(voice.language);
+    } catch (err) {
+      // My Samsung S9 has always this error: "Language is not supported"
+      console.log(`setDefaultLanguage error `, err);
+    }
     await Tts.setDefaultVoice(voice.id);
     this.setState({ selectedVoice: voice.id });
   };
@@ -84,34 +107,67 @@ export default class App extends Component<Props> {
   };
 
   render() {
+    /*
+     *       React Native TTS Example
+     *             |Read text|
+     *           Status: ready
+     *    Selected Voice: com.apple....
+     *      Speed: 0.50   ------o------
+     *      Pitch: 1.00   -----o-------
+     *  ________________________________
+     * | This is an example text        |
+     * |                                |
+     * |________________________________|
+     *           |de-DE - Anna|
+     *          |en-GB - Arthur|
+     *           |it-IT - Alice|
+     */
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>TTS Example</Text>
-        <Text style={styles.instructions}>{`Status: ${this.state.ttsStatus ||
+        <Text style={styles.title}>{`React Native TTS Example`}</Text>
+
+        <Button title={`Read text`} onPress={this.readText} />
+
+        <Text style={styles.label}>{`Status: ${this.state.ttsStatus ||
           ""}`}</Text>
-        <Text style={styles.instructions}>{`Selected Voice: ${this.state
+
+        <Text style={styles.label}>{`Selected Voice: ${this.state
           .selectedVoice || ""}`}</Text>
-        <Text
-          style={styles.instructions}
-        >{`Speed: ${this.state.speechRate.toFixed(2)}`}</Text>
-        <Slider
-          style={{ width: 300 }}
-          minimumValue={0.01}
-          maximumValue={0.99}
-          value={this.state.speechRate}
-          onSlidingComplete={this.setSpeechRate}
+
+        <View style={styles.sliderContainer}>
+          <Text
+            style={styles.sliderLabel}
+          >{`Speed: ${this.state.speechRate.toFixed(2)}`}</Text>
+          <Slider
+            style={styles.slider}
+            minimumValue={0.01}
+            maximumValue={0.99}
+            value={this.state.speechRate}
+            onSlidingComplete={this.setSpeechRate}
+          />
+        </View>
+
+        <View style={styles.sliderContainer}>
+          <Text
+            style={styles.sliderLabel}
+          >{`Pitch: ${this.state.speechPitch.toFixed(2)}`}</Text>
+          <Slider
+            style={styles.slider}
+            minimumValue={0.5}
+            maximumValue={2}
+            value={this.state.speechPitch}
+            onSlidingComplete={this.setSpeechPitch}
+          />
+        </View>
+
+        <TextInput
+          style={styles.textInput}
+          multiline={true}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+          onSubmitEditing={Keyboard.dismiss}
         />
-        <Text
-          style={styles.instructions}
-        >{`Pitch: ${this.state.speechPitch.toFixed(2)}`}</Text>
-        <Slider
-          style={{ width: 300 }}
-          minimumValue={0.5}
-          maximumValue={2}
-          value={this.state.speechPitch}
-          onSlidingComplete={this.setSpeechPitch}
-        />
-        <Button title={`Read`} onPress={this.readText} />
+
         <FlatList
           keyExtractor={item => item.id}
           renderItem={this.renderVoiceItem}
@@ -131,14 +187,30 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: "center",
     margin: 10
   },
-  instructions: {
+  label: {
+    textAlign: "center"
+  },
+  sliderContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  sliderLabel: {
     textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    marginRight: 20
+  },
+  slider: {
+    width: 150
+  },
+  textInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    flex: 1,
+    width: "100%"
   }
 });
